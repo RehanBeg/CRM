61bbe935b901dc3ee597befc930e9d121bdcba60
Rehan Beg Mirza <rehanbeg.mirza@juventus.schule>
2018-11-29 16:35:51 +0100

    Initial push

---
+++ src/com/crm/Main.java
@@ -0,0 +1,16 @@
+package com.crm;
+
+import com.crm.business.*;
+
+public class Main {
+
+    public String foo;
+
+    public static void main(String[] args) {
+        System.out.println("--- CRM by R&D ---");
+
+        Customer c = new Customer();
+        //c.getSalutation(foo);
+        
+    }
+}
---
+++ src/com/crm/business/Customer.java
@@ -0,0 +1,51 @@
+package com.crm.business;
+
+public class Customer {
+
+    private int Id;
+    private String salutation;
+    private String surname;
+    private String prename;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String salutation, String surname, String prename) {
+        Id = id;
+        this.salutation = salutation;
+        this.surname = surname;
+        this.prename = prename;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getSalutation() {
+        return salutation;
+    }
+
+    public void setSalutation(String salutation) {
+        this.salutation = salutation;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPrename() {
+        return prename;
+    }
+
+    public void setPrename(String prename) {
+        this.prename = prename;
+    }
+}
