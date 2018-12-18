9960baa06b3abc1e8d75681579bacb09683e2de9
Rehan Beg Mirza <rehanbeg.mirza@juventus.schule>
2018-12-18 21:55:08 +0100

    UnitTesting implemented and Javadoc created

--- src/ch/team3/test/business/CustomerTest.java
+++ src/ch/team3/test/business/CustomerTest.java
@@ -1,5 +1,5 @@
 /**
- * UnitTestingClass for CustomerTest class
+ * UnitTesting CustomerTest class to output the customer
  *
  * @author Rehan Beg Mirza
  * @author Dominic Wüthrich
@@ -7,10 +7,39 @@
 
 package ch.team3.test.business;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import ch.team3.business.Customer;
+
 public class CustomerTest {
 
+	/**
+	 * Declare Variables
+	 */
+	private final int CUSTOMER_ID = 0;
+	private final String CUSTOMER_SALUTATION = "herr";
+    private final String CUSTOMER_FIRSTNAME_1 = "meister";
+    private final String CUSTOMER_LASTNAME_1 = "proper";
+    private Customer customer;
 
-    // Kunde erstellen Test
+    /**
+     * Initializes the customer
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        customer = new Customer(CUSTOMER_ID, CUSTOMER_SALUTATION, CUSTOMER_FIRSTNAME_1, CUSTOMER_LASTNAME_1);
+    }
 
-
+    /**
+     * Checks if the added customer is same as the hard created and added customer
+     */
+    @Test
+    public void getDisplayName() {
+        assertThat(customer.getDisplayName(), is(this.CUSTOMER_SALUTATION + " " + this.CUSTOMER_FIRSTNAME_1 + " " + this.CUSTOMER_LASTNAME_1));
+    }
 }
--- src/ch/team3/test/persistence/CustomerDAOTest.java
+++ src/ch/team3/test/persistence/CustomerDAOTest.java
@@ -1,5 +1,5 @@
 /**
- * UnitTestingClass for CustomerDAOTest class
+ * UnitTesting CustomerDAOTest class to add, save and output the customer(s)
  *
  * @author Rehan Beg Mirza
  * @author Dominic Wüthrich
@@ -7,11 +7,45 @@
 
 package ch.team3.test.persistence;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.team3.persistence.CustomerDAO;
+
 public class CustomerDAOTest {
 
+	/**
+	 * Declare Variables
+	 */
+	private final int CUSTOMER_ID = 0;
+	private final String CUSTOMER_SALUTATION = "herr";
+    private final String CUSTOMER_FIRSTNAME_1 = "meister";
+    private final String CUSTOMER_LASTNAME_1 = "proper";
+    private final List<String> list = new ArrayList<>();
+    private CustomerDAO customerDAO;
 
+    /**
+     * Initializes the customerDAO and adds and saves the customer in a list
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        customerDAO = new CustomerDAO();
+        customerDAO.addCustomer(CUSTOMER_ID, CUSTOMER_SALUTATION, CUSTOMER_FIRSTNAME_1, CUSTOMER_LASTNAME_1);
+        list.add(CUSTOMER_ID + " " + CUSTOMER_SALUTATION + " " + CUSTOMER_FIRSTNAME_1 + " " + CUSTOMER_LASTNAME_1);
+    }
 
-    // Kunde anzeigen Test
-
-
+    /**
+     * Checks if the added and saved customer in DAO is same as the hard created and added customer in list
+     */
+    @Test
+    public void getCustomer() {
+        assertThat(customerDAO.getCustomers(), is(this.list));
+    }
 }
