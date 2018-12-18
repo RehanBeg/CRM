a58ecf3eaca96b04457f65bf0deca6c12e5cebb0
xXx-DoMoDo-xXx <43957849+xXx-DoMoDo-xXx@users.noreply.github.com>
2018-12-18 20:29:48 +0100

    UnitTest initial, vorbereitet

---
+++ src/ch/team3/test/business/CustomerTest.java
@@ -0,0 +1,16 @@
+/**
+ * UnitTestingClass for CustomerTest class
+ *
+ * @author Rehan Beg Mirza
+ * @author Dominic Wüthrich
+ */
+
+package ch.team3.test.business;
+
+public class CustomerTest {
+
+
+    // Kunde erstellen Test
+
+
+}
---
+++ src/ch/team3/test/persistence/CustomerDAOTest.java
@@ -0,0 +1,17 @@
+/**
+ * UnitTestingClass for CustomerDAOTest class
+ *
+ * @author Rehan Beg Mirza
+ * @author Dominic Wüthrich
+ */
+
+package ch.team3.test.persistence;
+
+public class CustomerDAOTest {
+
+
+
+    // Kunde anzeigen Test
+
+
+}
